9d2292cad6bcb1d1121496edba6ef115c28756f1
Jim Mueller <jimueller@users.noreply.github.com>
2015-09-07 11:11:42 -0500

    Create CNAME file during build process and cleanup.

--- scripts/gh-pages-deploy.sh
+++ scripts/gh-pages-deploy.sh
@@ -1,22 +1,19 @@
 #!/usr/bin/env bash
 set -e # halt script on error
 
-echo "Deploying -- variables"
-echo "Commit Range: ${TRAVIS_COMMIT_RANGE}"
-echo "Build: ${TRAVIS_BUILD_NUMBER}"
-echo "Git Deploy Target: ${GIT_DEPLOY_TARGET}"
-echo "Git Remote Branch: ${GIT_REMOTE_BRANCH}"
-echo ${GIT_REMOTE_BRANCH}
-
-
 cd _site
+
+# Add Github Pages CNAME file
+echo ${BASE_URL} > CNAME
+
+# Commit and push built site to gh-pages branch
 git init
 
 git config user.name "Travis CI"
 git config user.email "jimueller@users.noreply.github.com"
 
 git add .
-git commit -m "Deploy - Commit: ${TRAVIS_COMMIT_RANGE}, Build: ${TRAVIS_BUILD_NUMBER}"
+git commit -m "CIDeploy- Build:${TRAVIS_BUILD_NUMBER}, Commits:${TRAVIS_COMMIT_RANGE}"
 
 # We redirect any output to
 # /dev/null to hide any sensitive credential data that might otherwise be exposed.
